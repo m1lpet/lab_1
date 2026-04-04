a45fa0703a650324852c3ad5220c8af84a16b1f8
m1lpet <annatolstova06@gmail.com>
2026-04-04 15:31:06 +0300

    Update BoardGame.h

--- BoardGame.h
+++ BoardGame.h
@@ -38,34 +38,4 @@ public:
     void readBinary(std::istream& is);
 };
 
-class Database {
-private:
-    BoardGame* items;  
-    int count;         
-    int capacity;      
-
-    void resize(int newCapacity);  
-
-public:
-    Database();
-    ~Database();
-
-    Database(const Database&) = delete;
-    Database& operator=(const Database&) = delete;
-
-    void print() const;                   
-    void add(const BoardGame& bg);       
-    bool remove(int index);              
-    bool edit(int index, const BoardGame& bg); 
-    int* search(const std::string& name, const std::string& genre, int& resultCount) const;
-
-    bool saveToFile(const std::string& filename) const;
-    bool loadFromFile(const std::string& filename);
-
-    BoardGame& operator[](int index);
-    const BoardGame& operator[](int index) const;
-
-    int size() const;
-};
-
 #endif
